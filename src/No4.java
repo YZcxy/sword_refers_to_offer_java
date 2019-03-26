1c22f3e309ef8b38a6409a5e98ef1a4111fea368
Chad.Y <278253098@qq.com>
2019-03-26 10:03:48 +0800

    Update No4.java

--- src/No4.java
+++ src/No4.java
@@ -1,4 +1,7 @@
-//替换空格
+/**
+* 【替换空格】
+* 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+*/
 public class No4 {
     public String replaceSpace(StringBuffer str) {
         StringBuffer res = new StringBuffer();
