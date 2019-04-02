125dbe6e85dc498d34ff7d634c8f76ed89b9b2ec
Chad.Y <278253098@qq.com>
2019-04-02 10:55:18 +0800

    Update No7.java

--- src/No7.java
+++ src/No7.java
@@ -1,7 +1,9 @@
-//用两个栈实现队列
-
 import java.util.Stack;
 
+/**
+* 【用两个栈实现队列】
+* 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+*/
 public class No7 {
     Stack<Integer> stack1 = new Stack<Integer>();
     Stack<Integer> stack2 = new Stack<Integer>();
