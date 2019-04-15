8331771b9561457742f7576e2cba8690c33017c3
Chad.Y <278253098@qq.com>
2019-04-15 11:12:36 +0800

    Update No13.java

--- src/No13.java
+++ src/No13.java
@@ -1,4 +1,7 @@
-//在O(1)时间删除链表节点
+/**
+* 在O(1)时间删除链表节点
+* 给定单向链表的头指针和一个结点指针，在O(1)时间删除该结点。
+*/
 public class No13 {
     public void deleteNode(ListNode head,ListNode delNode){
         if(head==null||delNode==null)
